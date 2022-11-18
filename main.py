7c58e53702e0cf8b67242894eac4b6428fb4f1a2
FadedAditya <vishwakarmaaditya03@gmail.com>
2022-11-18 23:34:18 +0530

    my commit

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, render_template, url_for, request
 import csv
-app=Flask(__name__)
+
+app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
+
 def write_to_txt(data):
-    with open("database.txt",mode="a",newline="") as database:
-        email=data["email"]
+    with open("database.txt", mode="a", newline="") as database:
+        email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        file1=database.write(f"\n{email},{subject},{message}")
+        file1 = database.write(f"\n{email},{subject},{message}")
+
 
 def write_to_csv(data):
-    with open("database.csv",mode="a",newline="") as database:
+    with open("database.csv", mode="a", newline="") as database:
         writer = csv.writer(database)
-        writer.writerows(data)
-
-
-
+        writer.writerow([data["email"], data["subject"], data["message"]])
 
 
 @app.route('/thank', methods=['GET', 'POST'])
 def submit_message():
     if request.method == 'POST':
-        data=request.form.to_dict()
-        write_to_txt(data)
+        data = request.form.to_dict()
+        write_to_csv(data)
         return render_template('thank.html')
     else:
         return render_template('index.html')
